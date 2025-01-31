a154eae6c7d7a00c749a17a36fb20b6de14932d1
Razon Hassan <79102958+hassanrajon@users.noreply.github.com>
2025-02-01 02:28:46 +0600

    daily practice 01-02-2025

---
+++ B_Cat_Cycle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define debug(a) cerr << #a <<" = "<< (a) << '\n';
+#define nl cout<<'\n';
+void solve()
+{ 
+    int n,k;cin>>n>>k;
+    k--;
+    int skip = n/2;
+    cout << ((k + (n % 2) * k / skip) % n + 1) << '\n';
+}
+int main()
+{   ios_base::sync_with_stdio(0);cin.tie(0);
+    ll tt=1;
+    cin >> tt;
+    while (tt--){
+        solve();
+     }
+}
