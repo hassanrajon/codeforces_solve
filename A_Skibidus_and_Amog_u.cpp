a179c1adbcdba7adcc12cc3112dd94801016cc42
Razon Hassan <79102958+hassanrajon@users.noreply.github.com>
2025-02-10 13:30:48 +0600

    codeforces round 1003 div-4 solutioin a-e

---
+++ A_Skibidus_and_Amog_u.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define debug(a) cerr << #a <<" = "<< (a) << '\n';
+#define nl cout<<'\n';
+void solve()
+{ 
+  string s;cin>>s;
+  s.pop_back();
+  s.pop_back();
+  s.push_back('i');
+  cout<<s<<'\n';
+}
+int main()
+{   ios_base::sync_with_stdio(0);cin.tie(0);
+    ll tt=1;
+    cin >> tt;
+    while (tt--){
+        solve();
+     }
+}
